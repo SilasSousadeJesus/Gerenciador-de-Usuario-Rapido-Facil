9c50b479cbeee7f0f54c6bbfc14f7d896075e8ac
SilasSousadeJesus <silassousadejesus@gmail.com>
2026-02-22 12:42:13 -0300

    ajustando producers

--- Gerenciado-de-Usuario-Rapido-Facil.Infra/Migrations/CreateCopiaServicoSubtipoParaCotacaoProcedure.cs
+++ Gerenciado-de-Usuario-Rapido-Facil.Infra/Migrations/CreateCopiaServicoSubtipoParaCotacaoProcedure.cs
@@ -11,25 +11,23 @@ namespace Gerenciado_de_Usuario_Rapido_Facil.Infra.Migrations
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.Sql(@"
-            CREATE PROCEDURE IF NOT EXISTS CopiaServicoSubtipoParaCotacao()
-            BEGIN
-                -- Copiar Servicos
-                INSERT INTO cotacaorapidofacil.servicos (Id, Nome)
-                SELECT s.Id, s.Nome
-                FROM gerenciadorusuario.servicos s
-                WHERE NOT EXISTS (
-                    SELECT 1 FROM cotacaorapidofacil.servicos cs WHERE cs.Id = s.Id
-                );
-                
-                -- Copiar ServicosSubtipos
-                INSERT INTO cotacaorapidofacil.servicoSubtipos (Id, Nome, ServicoId)
-                SELECT st.Id, st.Nome, st.ServicoId
-                FROM gerenciadorusuario.servicoSubtipos st
-                WHERE NOT EXISTS (
-                    SELECT 1 FROM cotacaorapidofacil.servicoSubtipos cst WHERE cst.Id = st.Id
-                );
-            END;
-        ");
+                    CREATE PROCEDURE IF NOT EXISTS CopiaServicoSubtipoParaCotacao()
+                    BEGIN
+                        INSERT INTO `cotacaorapidofacil`.`Servicos` (Id, Nome)
+                        SELECT s.Id, s.Nome
+                        FROM `gerenciadorusuario`.`Servicos` s
+                        WHERE NOT EXISTS (
+                            SELECT 1 FROM `cotacaorapidofacil`.`Servicos` cs WHERE cs.Id = s.Id
+                        );
+
+                        INSERT INTO `cotacaorapidofacil`.`ServicoSubtipos` (Id, Nome, ServicoId)
+                        SELECT st.Id, st.Nome, st.ServicoId
+                        FROM `gerenciadorusuario`.`ServicoSubtipos` st
+                        WHERE NOT EXISTS (
+                            SELECT 1 FROM `cotacaorapidofacil`.`ServicoSubtipos` cst WHERE cst.Id = st.Id
+                        );
+                    END;
+                ");
         }
 
         /// <inheritdoc />
